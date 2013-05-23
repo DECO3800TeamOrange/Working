0a94ef0db0f78318d4096928ba1b1cd489000bc8
Heming <sun.h.ming@gmail.com>
2013-05-23 11:19:33 +1000

    Tab enabled
    tab enabled for search/sell switching, style/theme change is on the way.
    was figuring out how to apply complete set of matching style.

--- StudentSaleApp/src/com/example/studentsaleapp/Home.java
+++ StudentSaleApp/src/com/example/studentsaleapp/Home.java
@@ -2,14 +2,41 @@ package com.example.studentsaleapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
+import android.widget.TabHost.TabSpec;
 
-public class Home extends Activity {
+public class Home extends TabActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+		
+		TabHost tabHost = getTabHost();
+        
+        // Tab for searching
+        TabSpec searchspec = tabHost.newTabSpec("Search");
+        Intent searchIntent = new Intent(this, Search.class);
+        searchspec.setContent(searchIntent);
+        searchspec.setIndicator("Search");
+         
+        // Tab for Selling
+        TabSpec sellspec = tabHost.newTabSpec("Sell");        
+        Intent sellIntent = new Intent(this, MainActivity.class);
+        sellspec.setContent(sellIntent);
+        sellspec.setIndicator("Sell");
+         
+         
+        // Adding all TabSpec to TabHost
+        tabHost.addTab(searchspec); // Adding search tab
+        tabHost.addTab(sellspec); // Adding sell tab
 	}
 
 	@Override
@@ -19,4 +46,4 @@ public class Home extends Activity {
 		return true;
 	}
 
-}
+}    
